70f3bc20008389642062148789d0e53973ae1ae3
Wolfgang Huber <wolfgang.huber@embl.org>
2024-11-02 13:54:37 +0100

    Somre more refinements

--- upload.sh
+++ upload.sh
@@ -5,5 +5,5 @@
 chmod 644 index.html
 find index_files -type d -exec chmod 755 {} \;
 find index_files -type f -exec chmod 644 {} \;
-rsync -rvlpt index.html index_files wh.css datatransfer.embl.de:/g/huber/www-huber/users/whuber/pca-animation/
+rsync -rvlpt index.html index_files wh.css pca-animation.gif datatransfer.embl.de:/g/huber/www-huber/users/whuber/pca-animation/
 
